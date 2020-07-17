7e9ac366ea3649e9d2cfdb2e4cf2795f128bd74d
kurth4cker <turanibrahim126@gmail.com>
2020-07-18 02:38:18 +0300

    info.py yi her programınızda kullanabilirsiniz.

---
+++ info.py
@@ -0,0 +1,16 @@
+def bilgileri_bas(mesaj):
+    mesaj2 = ' Tarik VARDAR '
+    mesaj3 = ' tarikvardar@gmail.com '
+
+    mesaj= mesaj.center(40, '*')
+    mesaj2 = mesaj2.center(40, '*')
+    mesaj3 = mesaj3.center(40, '*')
+
+    print()
+    print('*' * 40)
+    print(mesaj)
+    print('*' * 40)
+    print(mesaj2)
+    print(mesaj3)
+    print('*' * 40)
+    print()
--- terhis_hesapla.py
+++ terhis_hesapla.py
@@ -7,23 +7,9 @@
 
 import datetime
 import datedelta
+import info
 
-mesaj = ' Terhis Hesaplama '
-mesaj2 = ' Tarik VARDAR '
-mesaj3 = ' tarikvardar@gmail.com '
-
-mesaj= mesaj.center(40, '*')
-mesaj2 = mesaj2.center(40, '*')
-mesaj3 = mesaj3.center(40, '*')
-
-print()
-print('*' * 40)
-print(mesaj)
-print('*' * 40)
-print(mesaj2)
-print(mesaj3)
-print('*' * 40)
-print()
+info.bilgileri_bas(" Terhis Hesapla ")
 
 #Bir sevk tarihi bilgisi girilmesini istiyoruz
 tarih0 = input('Sevk Tarihini (Gun.Ay.Yıl) Giriniz : ')
